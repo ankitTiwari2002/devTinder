7ef20b8a1448794a4dd40c649ccce6a776b13479
Github15102002 <ankittiwari15102002@gmail.com>
2024-12-22 00:53:25 -0800

    deleted and updated user in database

--- src/app.js
+++ src/app.js
@@ -45,6 +45,33 @@ app.get("/feed", async (req, res) => {
   }
 });
 
+// delete user from the database
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    const user = await User.findByIdAndDelete(userId);
+    res.send("user deleted succesfully");
+  } catch (err) {
+    res.status(400).send("something went wrong");
+  }
+});
+
+//update user in database
+app.patch("/user", async (req, res) => {
+  const _id = req.body.userId;
+  const data = req.body;
+  try {
+    const user = await User.findByIdAndUpdate(_id, data);
+    if (!user) {
+      res.status(404).send("user not exist");
+    } else {
+      res.send("user updated succesfully");
+    }
+  } catch (err) {
+    res.status(400).send("something went wrong");
+  }
+});
+
 connectDB()
   .then(() => {
     console.log("DataBase connection established...");
