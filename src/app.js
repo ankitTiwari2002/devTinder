3c4fea00abb7a2477d45fd57934c79f7cc582379
Github15102002 <ankittiwari15102002@gmail.com>
2025-01-20 00:12:55 -0800

    created http server and initialized socket

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/database");
 const cors = require("cors");
+const http = require("http");
 const app = express();
 
 require("dotenv").config();
@@ -20,16 +21,20 @@ const authRouter = require("./routes/auth.js");
 const profileRouter = require("./routes/profile");
 const requestRouter = require("./routes/request");
 const userRouter = require("./routes/user.js");
+const initializedSocket = require("./utils/socket.js");
 
 app.use("/", authRouter);
 app.use("/", profileRouter);
 app.use("/", requestRouter);
 app.use("/", userRouter);
 
+const server = http.createServer(app);
+initializedSocket(server);
+
 connectDB()
   .then(() => {
     console.log("DataBase connection established...");
-    app.listen(process.env.PORT, () => {
+    server.listen(process.env.PORT, () => {
       console.log("server is listning on port 7777");
     });
   })
