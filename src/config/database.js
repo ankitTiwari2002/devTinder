7964eee80483ad55ace36ac50b866a6accbd3593
Github15102002 <ankittiwari15102002@gmail.com>
2025-08-31 14:45:41 +0530

    removed comment

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
-//mongodb+srv://ankittiwari6705:<db_password>@namastenode.k69xq.mongodb.net/
 const connectDB = async () => {
   await mongoose.connect(process.env.DB_CONNECTION_STRING);
 };
